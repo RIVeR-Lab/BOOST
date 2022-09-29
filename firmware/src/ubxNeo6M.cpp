70232f73926517294ced6254c9dff4db368c966e
David Antaki <antakidavid@gmail.com>
2022-09-29 19:21:55 -0400

    Delete dead code

--- firmware/src/ubxNeo6M.cpp
+++ firmware/src/ubxNeo6M.cpp
@@ -19,24 +19,15 @@ ubxNeo6M::~ubxNeo6M() {}
   * -EBUSY – If async reception was enabled using uart_rx_enable
   */
 int ubxNeo6M::readIn() {
-  bool success = true;
   unsigned char inChar;
 
   int stat = uartPort.poll_read(inChar);
   // If char arrived, add to buffer and keep reading until no more characters to read.
   while (stat == 0) {
     stat = uartPort.poll_read(inChar);
-
     // Add character to the encoder
     gpsEncoder.encode(inChar);
-    // memcpy(&rxBuffer[rxBufferI++], &inChar, 1);
-    // // If buffer is full, print it out
-    // if (rxBufferI >= rxBufferLen) {
-    //   rxBufferI = 0;
-    //   LOG_DBG("\n%s\r\n", log_strdup(rxBuffer));
-    // }
   }
 
-  success = success && (stat == 0 || stat == -1);
   return stat;
 }
