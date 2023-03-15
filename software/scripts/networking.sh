c29ed7f5bdb296136401fbdab5420477384cfc01
David Antaki <antakidavid@gmail.com>
2023-03-15 17:27:15 -0400

    Add user jetson to dialout group
    So that it can access tty devices

--- software/scripts/networking.sh
+++ software/scripts/networking.sh
@@ -2,6 +2,7 @@
 echo "Launching roscore, ROS1 Bridge and rosserial on Jetson"
 source /opt/ros/galactic/setup.bash
 source /opt/ros/noetic/setup.bash
+sudo adduser jetson dialout
 roscore & sleep 2 && rosrun rosserial_python serial_node.py _port:=/dev/ttyACM0 _baud:=57600 & sleep 2 && ros2 run ros1_bridge dynamic_bridge --bridge-all-1to2-topics
 
 # start roscore.. Sleep 2 seconds .. start rosserial ... sleep 2 seconds ... run ros bridge
