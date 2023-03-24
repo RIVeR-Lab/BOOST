75d6d3a9c32f87f818d708f8dc84fe481b9c30b9
David Antaki <antakidavid@gmail.com>
2023-03-24 15:46:30 -0400

    Increase IMU publishing freq to 40hz

--- firmwarePioCube/src/version_AUTO_GENERATED/version.cpp
+++ firmwarePioCube/src/version_AUTO_GENERATED/version.cpp
@@ -1,8 +1,8 @@
 
 #include "version.h"
 std::string Version::getGitCommitSha1() {
- return "55ddab3d3a0e0e91378997b2584e27f747f62d9d";
+ return "40385e37f7422d1b2d0496ae825437bc7578b1f0";
 }
 std::string Version::getBuildTimestamp() {
- return "2023-03-20 17:45:25.094061";
+ return "2023-03-24 15:45:49.850217";
 }
