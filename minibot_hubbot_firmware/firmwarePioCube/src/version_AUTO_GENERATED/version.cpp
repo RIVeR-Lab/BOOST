30d04b138cd950881892691b63c5d824362aad68
David Antaki <antakidavid@gmail.com>
2023-04-08 15:25:06 -0400

    Add firmware release

--- minibot_hubbot_firmware/firmwarePioCube/src/version_AUTO_GENERATED/version.cpp
+++ minibot_hubbot_firmware/firmwarePioCube/src/version_AUTO_GENERATED/version.cpp
@@ -1,8 +1,8 @@
 
 #include "version.h"
 std::string Version::getGitCommitSha1() {
- return "e52dc02c76ae37903a4874b59c5794d6be0e7d2a";
+ return "258e8f69f2fc50367389532061ff9d8a270ad31f";
 }
 std::string Version::getBuildTimestamp() {
- return "2023-04-08 15:23:10.057046";
+ return "2023-04-08 15:24:04.325026";
 }
