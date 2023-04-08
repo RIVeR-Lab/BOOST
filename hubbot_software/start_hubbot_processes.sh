e7ae540706fe85cf7ab2639f13fd3a988f3a872f
davidantaki <antakidavid@gmail.com>
2023-04-09 01:00:08 +0200

    Update start script to only by the one node

--- hubbot_software/start_hubbot_processes.sh
+++ hubbot_software/start_hubbot_processes.sh
@@ -12,9 +12,11 @@ echo $VERBOSE
 
 echo "Starting ChargeController..."
 if [[ "$VERBOSE" -eq 1 ]]; then
-  python3 $BASEPATH/charge_controller.py &
+  # python3 $BASEPATH/charge_controller.py &
+  sudo colcon build && source install/setup.sh && ros2 run py_hubbot_main_controller_node hubbot_main_controller_node
 else
-  python3 $BASEPATH/charge_controller.py > /dev/null 2>&1 &
+  # python3 $BASEPATH/charge_controller.py > /dev/null 2>&1 &
+  sudo colcon build && source install/setup.sh && ros2 run py_hubbot_main_controller_node hubbot_main_controller_node
 fi
 
 echo "DONE"
