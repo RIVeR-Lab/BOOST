1e0f4552b8d54661d7bc11edf76b4f3dcb2b70e0
davidantaki <antakidavid@gmail.com>
2023-04-08 20:38:23 +0200

    Publishing hub status working

--- hubbot_software/ros2_ws/src/py_hubbot_status_pubsub/py_hubbot_status_pubsub/hubbot_status_publisher.py
+++ hubbot_software/ros2_ws/src/py_hubbot_status_pubsub/py_hubbot_status_pubsub/hubbot_status_publisher.py
@@ -14,22 +14,26 @@
 
 import rclpy
 from rclpy.node import Node
-
 from std_msgs.msg import String
+from std_msgs.msg import Int32
+from .bot_stats import HubbotStats, MinibotStats
+
 
 
 class HubbotStatusPublisher(Node):
+    PUBLISH_PERIOD_S = 0.5
+    current_stat: HubbotStats.STAT = HubbotStats.STAT.HubNotReady
 
     def __init__(self):
-        super().__init__('minimal_publisher')
-        self.publisher_ = self.create_publisher(String, 'topic', 10)
-        timer_period = 0.5  # seconds
+        super().__init__('HubbotStatPub')
+        self.publisher_ = self.create_publisher(Int32, 'hubstatus', 10)
+        timer_period = self.PUBLISH_PERIOD_S
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
 
     def timer_callback(self):
-        msg = String()
-        msg.data = 'Hello World: %d' % self.i
+        msg = Int32()
+        msg.data = self.current_stat.value
         self.publisher_.publish(msg)
         self.get_logger().info('Publishing: "%s"' % msg.data)
         self.i += 1
--- shared/bot_stats.py
+++ shared/bot_stats.py
@@ -1,33 +1,39 @@
 from std_msgs.msg import Int32
-from enum import Enum
+from enum import IntEnum, auto
 
 
 class HubbotStats:
-    # Stats
-    STATS = Enum('HubbotStats',
-                 ['HubReadyForMinibotDocking',
-                  'HubReadyForMinibotUndocking',
+  class STAT(IntEnum):
+    HubNotReady = auto()
+    HubReadyForMinibotDocking = auto()
+    HubReadyForMinibotUndocking = auto()
 
-                  ])
-    # @classmethod
-    # def get
-    # def __init__(self):
+  
+
+  @classmethod
+  def ros_msg_to_stat(msg: Int32) -> STAT:
+    print("msg" + msg)
+    return STAT(msg.data)
+
+
+  @classmethod
+  def stat_to_ros_msg(stat: STAT) -> Int32:
+    print("state" + stat)
+    return Int32(stat.value)
 
 
 class MinibotStats:
-    # Stats
-    STATS = Enum('MinibotStates',
-                 ['MiniBattLow',
-                  'MiniDocked',
-
-                  ])
+  class STAT(IntEnum):
+    MiniBattLow = auto()
+    MiniDocked = auto()
     # @classmethod
     # def get
     # def __init__(self):
 
 
 def main():
-  print(HubbotStats.STATS.HubReadyForMinibotDocking)
+    print(HubbotStats.STATS.HubReadyForMinibotDocking)
+
 
 if __name__ == '__main__':
     main()
