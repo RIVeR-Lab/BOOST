258e8f69f2fc50367389532061ff9d8a270ad31f
David Antaki <antakidavid@gmail.com>
2023-04-08 15:23:26 -0400

    Use macro and change name to minibot_a

--- minibot_hubbot_firmware/firmwarePioCube/src/version_AUTO_GENERATED/version.cpp
+++ minibot_hubbot_firmware/firmwarePioCube/src/version_AUTO_GENERATED/version.cpp
@@ -1,8 +1,8 @@
 
 #include "version.h"
 std::string Version::getGitCommitSha1() {
- return "b83fca1967bb3530df17838c301204a25c2ed255";
+ return "e52dc02c76ae37903a4874b59c5794d6be0e7d2a";
 }
 std::string Version::getBuildTimestamp() {
- return "2023-04-08 11:25:31.481238";
+ return "2023-04-08 15:23:10.057046";
 }
