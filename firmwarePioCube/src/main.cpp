6554c957071d881ce0c70ef1aba18762b7ca6cf3
David Antaki <antakidavid@gmail.com>
2023-04-08 09:47:45 -0400

    comment out the encoder testing stuf

--- firmwarePioCube/src/main.cpp
+++ firmwarePioCube/src/main.cpp
@@ -43,33 +43,33 @@ void interrupt_func() {
 void setup() {
   // console6.setTx(SERIAL_CONSOLE_TX_PIN);
   // console6.setRx(SERIAL_CONSOLE_RX_PIN);
-  Serial.begin(57600);
-  while (!Serial) {
-    yield();
-  }
+  // Serial.begin(57600);
+  // while (!Serial) {
+  //   yield();
+  // }
 
-  pinMode(PB5, OUTPUT);
-  digitalWrite(PB5, HIGH);
+  // pinMode(PB5, OUTPUT);
+  // digitalWrite(PB5, HIGH);
 
   // attachInterrupt(PA0, interrupt_func, CHANGE);
-  // realMain.init();
-  // realMain.loop();
+  realMain.init();
+  realMain.loop();
 
   // HardwareTimer *MyTim = new HardwareTimer(TIM5);
   // MyTim->setOverflow(1, HERTZ_FORMAT); // 10 Hz
   // MyTim->attachInterrupt(Update_IT_callback);
   // MyTim->resume();
 }
-long oldPosition  = -999;
-Encoder leftQuadEnc(PA0, PA1);
-Encoder rightQuadEnc(PA5, PB3);
+// long oldPosition  = -999;
+// Encoder leftQuadEnc(PA0, PA1);
+// Encoder rightQuadEnc(PA5, PB3);
 
 void loop() {
-  int32_t leftPos = leftQuadEnc.read();
-  Serial.printf("LeftEnc: %d\n\r", leftPos);
-  int32_t rightPos = rightQuadEnc.read();
-  Serial.printf("RightEnc: %d\n\r", rightPos);
-  delay(1000);
+  // int32_t leftPos = leftQuadEnc.read();
+  // Serial.printf("LeftEnc: %d\n\r", leftPos);
+  // int32_t rightPos = rightQuadEnc.read();
+  // Serial.printf("RightEnc: %d\n\r", rightPos);
+  // delay(1000);
   // Execution should not get here!!
   Serial.print("!!!!!!!!!Execution should not get here!!!!!!!!!\n\r");
 }
